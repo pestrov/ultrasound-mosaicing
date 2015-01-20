e64f3f7f307b540d97c6c77c5e74c8a7897470f2
Nikita Pestrov <ipestrov@gmail.com>
2015-01-20 17:41:25 +0300

    Fixed finding if the patch is good enough

--- Matlab/getImagePatches.m
+++ Matlab/getImagePatches.m
@@ -17,52 +17,42 @@ bestPatch = 0;
 secondBestDiff = 0;
 secondBestPatch = 0;
 difference = intmax;
+secondDiff = intmax;
 
 %Patch matching params
-windowWidth = 50;
-patchSize = 80;
- 
+windowWidth = 30;
+patchSize = 32;
+patchArea = patchSize*patchSize;
+patchIntersectionThreshold = patchArea*0.3;
+
 xTranslation = radius * (cosd(angleChange*imageNumber) - cosd(angleChange*(imageNumber - 1)))
 yTranslation = radius * (sind(angleChange*imageNumber) - sind(angleChange*(imageNumber - 1)))
 
 for firstXShift = -windowWidth:5:windowWidth
     for firstYShift = -windowWidth:5:windowWidth
-        firstXShift = 0;
-        firstYShift = 0;
+        
         %Create patch from first image
         patchX = imageSize1(1)/2 + firstXShift - patchSize/2;
         patchY = imageSize1(2)/2 + firstYShift - patchSize/2;
-        patchRect = [patchX patchY patchSize patchSize]
+        patchRect = [patchX patchY patchSize patchSize];
         firstImagePatch = imcrop(cropedFirstImage, patchRect);
         
         bestPatch = 0;
-
-        for xShift = -windowWidth:5:windowWidth
-            for yShift = -windowWidth:5:windowWidth
-                xShift = 0;
-                yShift = 0;
-                
+        
+        for xShift = -windowWidth:2:windowWidth
+            for yShift = -windowWidth:2:windowWidth
                 %Create patch from second image
                 secondPatchX = secondImageXCenter + xShift - xTranslation - patchSize/2;
                 secondPatchY = secondImageYCenter + yShift - yTranslation - patchSize/2;
-                secondPatchRect = [secondPatchX secondPatchY patchSize patchSize]
+                secondPatchRect = [secondPatchX secondPatchY patchSize patchSize];
                 secondImagePatch = imcrop(cropedSecondImage, secondPatchRect);
+                %                 imshow(firstImagePatch), figure, imshow(secondImagePatch);
+                %                 pause(15);
                 
-%                 imshow(firstImagePatch), figure, imshow(secondImagePatch);
-%                 pause(15);
-    
                 if size(firstImagePatch) == size(secondImagePatch)
                     currentDifference = sum(sum(imabsdiff(firstImagePatch, secondImagePatch)));
                     if currentDifference < difference
-                        if bestPatch
-                           if (rectint(bestPatchRect, secondPatchRect) < 0.5) && (currentDifference < 0.8 * difference)
-                                secondBestPatch = bestPatch;
-                                secondBestPatchRect = bestPatchRect;
-                           else
-                               secondBestPatch = 0;
-                               secondBestPatchRect = 0;
-                           end
-                        end
+                        %Saving best result
                         bestPatch = secondImagePatch;
                         bestPatchRect = secondPatchRect;
                         difference = currentDifference;
@@ -71,26 +61,55 @@ for firstXShift = -windowWidth:5:windowWidth
             end
         end
         
-        if secondBestPatch
-            finalFirstImagePatch = firstImagePatch;
-            finalBestPatch = bestPatch;
-            finalSecondBestPatch = secondBestPatch;
-            finalBestPatchRect = bestPatchRect;
-            finalSecondBestPatchRect = secondBestPatchRect;
-            finalFirstPatchRect = patchRect;
-            finalBestPatchRect;
-            detectedTranforms = [detectedTranforms,{[(patchRect(1) - firstImageXCenter) - (bestPatchRect(1) - secondImageXCenter) (patchRect(2) - firstImageYCenter) - (bestPatchRect(2) - secondImageYCenter)]}];
+        patchIsNotGoodEnough = 0;
+        
+        for xShift = -windowWidth:2:windowWidth
             
+            if patchIsNotGoodEnough
+                break;
+            end
+            
+            for yShift = -windowWidth:2:windowWidth
+                %Create patch from second image
+                secondPatchX = secondImageXCenter + xShift - xTranslation - patchSize/2;
+                secondPatchY = secondImageYCenter + yShift - yTranslation - patchSize/2;
+                secondPatchRect = [secondPatchX secondPatchY patchSize patchSize];
+                secondImagePatch = imcrop(cropedSecondImage, secondPatchRect);
+                
+                if size(firstImagePatch) == size(secondImagePatch)
+                    currentDifference = sum(sum(imabsdiff(firstImagePatch, secondImagePatch)));
+                    
+                    %Check if the new rect is really far away
+                    if rectint(bestPatchRect, secondPatchRect) < patchIntersectionThreshold
+                        if difference > 0.9 * currentDifference
+                            patchIsNotGoodEnough = 1;
+                            break;
+                        end
+                    end
+                end
+            end
         end
-        difference = intmax;
-        currentDifference = intmax;
-        bestPatch = 0;
     end
+    
+    if patchIsNotGoodEnough == 0
+        finalFirstImagePatch = firstImagePatch;
+        finalBestPatch = bestPatch;
+        finalBestPatchRect = bestPatchRect;
+        finalFirstPatchRect = patchRect;
+        detectedTranforms = [detectedTranforms,{[(patchRect(1) - firstImageXCenter) - (bestPatchRect(1) - secondImageXCenter) (patchRect(2) - firstImageYCenter) - (bestPatchRect(2) - secondImageYCenter)]}];
+        
+    end
+    difference = intmax;
+    currentDifference = intmax;
+    bestPatch = 0;
+end
+celldisp(detectedTranforms);
 end
 
 %            bestFirstPatch = firstImagePatch;
 %imshow(finalFirstImagePatch), figure, imshow(finalBestPatch), figure, imshow(finalSecondBestPatch), figure, imshow(cropedFirstImage), figure,imshow(cropedSecondImage) ;
- finalBestPatchRect
- secondBestPatchRect
- finalFirstPatchRect
- celldisp(detectedTranforms);
+%  finalBestPatchRect
+%  secondBestPatchRect
+%  finalFirstPatchRect
+
+ 
